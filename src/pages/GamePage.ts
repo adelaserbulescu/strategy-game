eefb2220e8f34723e07036b4b6fe66e64e875c57
Adela Serbulescu <adela.serbulescu@student.upt.ro>
2026-01-17 21:31:00 +0200

    Deleted unorganized files

--- src/pages/GamePage.ts
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function GamePage() {
-  const { matchId } = useParams();
-  return <h1>Game {matchId}</h1>;
-}
--- src/pages/LobbyPage.ts
+++
@@ -1,3 +0,0 @@
-export default function LobbyPage() {
-  return <h1>Lobby</h1>;
-}
