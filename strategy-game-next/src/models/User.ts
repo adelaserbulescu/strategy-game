d306ba6d843aacbe56d9759b48854238f5d7ba89
Adela Serbulescu <adela.serbulescu@student.upt.ro>
2026-01-18 19:32:33 +0200

    Removed old subtree.

--- strategy-game-next/src/models/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  username: string;
-  description?: string;
-  gamesWon: number;
-  role: "PLAYER" | "ADMIN";
-  createdAt?: string;
-  updatedAt?: string;
-}
